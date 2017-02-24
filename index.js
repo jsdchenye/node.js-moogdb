f83b9f75ebdf6222c787674658f4c0c93f38822c
jsdchenye <jsdchenye@163.com>
2017-02-24 20:02:21 +0800

    学习使用express的中间件

--- index.js
+++ index.js
@@ -11,7 +11,20 @@ var userRouter = require('./routes/users');
 app.set('views',path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use('/', indexRouter);
-app.use('/users', userRouter);
+// app.use('/', indexRouter);
+// app.use('/users', userRouter);
+
+app.use(function(req,res,next) {
+	console.log('1');
+	next(new Error('hahhahh'));
+});
+app.use(function(req,res,next) {
+	console.error('2');
+	res.status(200).end();
+});
+app.use(function(err, req, res, next) {
+	console.error(err.stack);
+	res.status(500).send('Something broke!');
+});
 
 app.listen(3000);
